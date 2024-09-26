203ec62f089fcb7c67f5878821e59e9079491f7f
KarlW-JCU <karl.wellington@my.jcu.edu.au>
2024-09-26 14:21:23 +1000

    Add prac_02/extras, random score recording program and results.txt

---
+++ prac_02/extras/more_scores.py
@@ -0,0 +1,29 @@
+"""
+Random score recording program
+"""
+
+from random import randint
+
+
+def main():
+    scores = []
+    number_of_scores = int(input("Number of scores: "))
+    for i in range(number_of_scores):
+        scores.append(randint(0, 100))
+    file_out = open("results.txt", "w")
+    for score in scores:
+        print(f"{score} is {determine_performance(score)}", file=file_out)
+    file_out.close()
+    print("Done.")
+
+
+def determine_performance(score):
+    if score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
