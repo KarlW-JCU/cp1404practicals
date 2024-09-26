653ab247e4a8616c4cdc78e533a16fd3ee7bec15
KarlW-JCU <karl.wellington@my.jcu.edu.au>
2024-09-26 12:57:20 +1000

    Refactor score checking program into functions and add random score

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,14 +1,26 @@
 """
-CP1404/CP5632 - Practical
-Broken program to determine score status
+Score checking program
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+from random import randint
+
+
+def main():
+    score = float(input("Enter score: "))
+    print(f"Your score is {determine_performance(score)}.")
+    random_score = randint(0, 100)
+    print(f"Random score: {random_score} is {determine_performance(random_score)}.")
+
+
+def determine_performance(score):
+    if score < 0 or score > 100:
+        return "Invalid"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
