e9f87013ac3a1c6f8b1820dd1faf5c55a463f259
KarlW-JCU <karl.wellington@my.jcu.edu.au>
2024-09-26 12:33:26 +1000

    Refactor simple password program into functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,10 +1,23 @@
 """Simple password program"""
 
-
 MIN_LENGTH = 8
 
-password = input("Password: ")
-while len(password) < MIN_LENGTH:
-    print(f"Password must be at least {MIN_LENGTH} characters long.")
+
+def main():
+    password = get_password()
+    print_stars(password)
+
+
+def get_password():
     password = input("Password: ")
-print("Password: ", "*" * len(password), sep='')
+    while len(password) < MIN_LENGTH:
+        print(f"Password must be at least {MIN_LENGTH} characters long.")
+        password = input("Password: ")
+    return password
+
+
+def print_stars(string):
+    print("Password: ", "*" * len(string), sep='')
+
+
+main()
