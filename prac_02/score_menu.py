34f1f320b9df15c6d7a86d59ea1936e95bc5f339
KarlW-JCU <karl.wellington@my.jcu.edu.au>
2024-09-26 13:34:31 +1000

    Update prize awarding program with outputs for all menu options

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -5,28 +5,42 @@ Prize awarding program
 MENU = ("(S)core\n"
         "(R)esult\n"
         "(P)rize\n"
-        "(Q)uit\n")
+        "(Q)uit")
 
 
 def main():
-    score = int(input("Enter score: "))
-    while score < 0 or score > 100:
-        print("Score must be between 0 and 100")
-        score = int(input("Enter score: "))
+    score = get_score()
     print(MENU)
     selection = input(">>> ").upper()
     while selection != 'Q':
         if selection == 'S':
-            pass
+            score = get_score()
         elif selection == 'R':
-            pass
+            print(f"A score of {score} is {determine_performance(score)}.")
         elif selection == 'P':
-            pass
+            print("*" * score)
         else:
             print("Invalid selection")
         print(MENU)
         selection = input(">>> ").upper()
     print("Farewell")
+
+
+def get_score():
+    score = int(input("Enter score: "))
+    while score < 0 or score > 100:
+        print("Score must be between 0 and 100")
+        score = int(input("Enter score: "))
+    return score
+
+
+def determine_performance(score):
+    if score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
 
 
 main()
