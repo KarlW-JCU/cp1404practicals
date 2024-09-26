11b18857a5eb1ee1b4950f4b0e7dae9ef9276245
KarlW-JCU <karl.wellington@my.jcu.edu.au>
2024-09-26 15:19:31 +1000

    Modify convert_temps.py to include celsius to fahrenheit conversion

--- prac_02/extras/convert_temps.py
+++ prac_02/extras/convert_temps.py
@@ -1,5 +1,6 @@
 """
-Fahrenheit to Celsius file conversion program
+Temperature conversion program
+Reads from and Writes to file
 """
 
 
@@ -8,11 +9,18 @@ def main():
     file_out = open('temps_output.txt', 'w')
     for line in file_in:
         print(convert_fahrenheit_to_celsius(float(line)), file=file_out)
+        # print(convert_celsius_to_fahrenheit(float(line)), file=file_out)
     file_in.close()
     file_out.close()
     print('Done')
 
+
 def convert_fahrenheit_to_celsius(fahrenheit):
     return (fahrenheit - 32) * 5 / 9
+
+
+def convert_celsius_to_fahrenheit(celsius):
+    return celsius * 9 / 5 + 32
+
 
 main()
