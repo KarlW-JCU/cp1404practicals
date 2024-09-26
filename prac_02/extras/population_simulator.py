29bf4e001ef03d803c8b0c4234afb806086b151c
KarlW-JCU <karl.wellington@my.jcu.edu.au>
2024-09-26 15:13:27 +1000

    Add prac_02/extras population simulation program

---
+++ prac_02/extras/population_simulator.py
@@ -0,0 +1,28 @@
+"""
+Population simulation program
+"""
+
+from random import uniform
+
+MINIMUM_BIRTHS = 0.10
+MAXIMUM_BIRTHS = 0.20
+MINIMUM_DEATHS = 0.05
+MAXIMUM_DEATHS = 0.25
+
+
+def main():
+    population = []
+    species = input('Species Name: ').title()
+    print(f"Welcome to the {species} Population Simulator!")
+    population.append(int(input('Starting Population: ')))
+    print(population)
+    for i in range(0, 10):
+        print(f"Year {i + 1}")
+        births = round(uniform(MINIMUM_BIRTHS, MAXIMUM_BIRTHS) * population[i])
+        deaths = round(uniform(MINIMUM_DEATHS, MAXIMUM_DEATHS) * population[i])
+        print(f"{births} {species} were born. {deaths} died.")
+        population.append(population[i] + births - deaths)
+        print(f"Population: {population[i]}.")
+
+
+main()
