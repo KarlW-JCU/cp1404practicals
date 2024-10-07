dc17bc9a716b4e96561998427355fcc99100268c
KarlW-JCU <karl.wellington@my.jcu.edu.au>
2024-10-07 18:36:00 +1000

    Complete automatic password generator program

--- prac_03/extras/password_generator.py
+++ prac_03/extras/password_generator.py
@@ -2,28 +2,31 @@
 Automatic Password Generator Program.
 """
 
-MIN_LENGTH = 4
+import random
+
+MIN_LENGTH = 8
 MAX_LENGTH = 16
 IS_SPECIAL_CHARACTER_REQUIRED = True
-VOWELS = "aeiou"
-CONSONANTS = "bcdfghjklmnpqrstvwxyz"
+NUMBERS = "0123456789"
+LETTERS = "abcdefghijklmnopqrstuvwxyz"
 SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
 
 
 def main():
-    """Program to get and check a user's password."""
-    print("Please enter a valid password")
-    print(f"Your password must be between {MIN_LENGTH} and {MAX_LENGTH} characters, and contain:")
+    """Program to generate, check, and print a password."""
+    print("Generating Password...")
+    print(f"Your password will be between {MIN_LENGTH} and {MAX_LENGTH} characters, and contain:")
+    word_length = random.randint(MIN_LENGTH, MAX_LENGTH)
     print("\t1 or more uppercase characters")
     print("\t1 or more lowercase characters")
     print("\t1 or more numbers")
     if IS_SPECIAL_CHARACTER_REQUIRED:
-        print("\tand 1 or more special characters: ", SPECIAL_CHARACTERS)
-    password = input("> ")
+        print("\tand 1 or more special characters:", SPECIAL_CHARACTERS)
+    input("Press 'Enter' to continue...")
+    password = generate_password(word_length)
     while not is_valid_password(password):
-        print("Invalid password!")
-        password = input("> ")
-    print(f"Your {len(password)}-character password is valid: {password}")
+        password = generate_password(word_length)
+    print(f"Your {len(password)}-character password is: {password}")
 
 
 def is_valid_password(password):
@@ -50,6 +53,22 @@ def is_valid_password(password):
     if IS_SPECIAL_CHARACTER_REQUIRED and number_of_special == 0:
         return False
     return True
+
+
+def generate_password(word_length):
+    """Generate a random password."""
+    word = ""
+    for character in range(word_length):
+        random_character = random.random()
+        if random_character <= 0.25:
+            word += random.choice(LETTERS).upper()
+        elif random_character <= 0.50:
+            word += random.choice(LETTERS)
+        elif random_character <= 0.75:
+            word += random.choice(NUMBERS)
+        else:
+            word += random.choice(SPECIAL_CHARACTERS)
+    return word
 
 
 main()
