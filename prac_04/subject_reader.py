f2e9b9e2779e2511f123167ee6fd327fdeb5068e
KarlW-JCU <karl.wellington@my.jcu.edu.au>
2024-10-10 08:23:31 +1000

    Complete subject details program

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -1,29 +1,31 @@
-"""
-CP1404/CP5632 Practical
-Data file -> lists program
-"""
+"""Display subject details program."""
 
 FILENAME = "subject_data.txt"
 
 
 def main():
-    data = load_data()
-    print(data)
+    """Read data from file and print details formatted like: subject,lecturer,number of students."""
+    subject_data = load_data()
+    print_details(subject_data)
 
 
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    data = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        line = line.strip()
+        parts = line.split(',')
+        parts[2] = int(parts[2])  # ignore PyCharm's warning
+        data.append(parts)
     input_file.close()
+    return data
+
+
+def print_details(data):
+    """Print data formatted like: subject,lecturer,number of students."""
+    for item in data:
+        print(f"{item[0]} is taught by {item[1]} and has {item[2]} students.")
 
 
 main()
