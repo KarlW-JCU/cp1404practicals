ec584f790dc86cfe6d1991d8aa21aacd346a927d
KarlW-JCU <karl.wellington@my.jcu.edu.au>
2024-10-17 20:07:40 +1000

    Add sorted countries by number of finalists to Wimbledon program

--- prac_05/extras/more_wimbledon.py
+++ prac_05/extras/more_wimbledon.py
@@ -12,8 +12,9 @@ FILE = "..\\wimbledon.csv"
 def main():
     """Print summary of Wimbledon champions and their countries."""
     data = read_file()
-    champion_to_wins, champion_to_losses = assign_champion_to_record(data)
-    countries = get_unique_countries(data)
+    champion_to_wins = assign_champion_to_wins(data)
+    champion_to_losses = assign_champion_to_losses(data)
+    countries = count_countries(data)
     display_summary(champion_to_wins, champion_to_losses, countries)
 
 
@@ -24,36 +25,50 @@ def display_summary(champion_to_wins, champion_to_losses, countries):
         print(champion, score)
     print()
     print(f"These {len(countries)} have won Wimbledon:")
-    print(", ".join(countries))
+    for country, number_of_players in sorted(countries.items(), key=itemgetter(1), reverse=True):
+        print(f"{country}:{number_of_players}", end="; ")
+    print()
     print()
     print("Wimbledon Losses:")
     for champion, score in sorted(champion_to_losses.items(), key=itemgetter(1), reverse=True):
         print(champion, score)
 
 
-def get_unique_countries(data):
+def count_countries(data):
     """Return set of unique countries."""
-    countries = {line.split('"')[0].split(',')[1] for line in data}
+    countries = {}
+    for line in data:
+        for country in [line.split('"')[0].split(',')[1], line.split('"')[0].split(',')[3]]:
+            try:
+                countries[country] += 1
+            except KeyError:
+                countries[country] = 1
     return countries
 
 
-def assign_champion_to_record(data):
-    """Return dictionary with champion name to number of wins and losses."""
+def assign_champion_to_wins(data):
+    """Return dictionary with champion name to number of wins."""
     champion_to_wins = {}
-    champion_to_losses = {}
     for line in data:
-        score_digits = [int(char) for char in line.split('"')[1] if char in '1234567890']
         try:
             champion_to_wins[line.split('"')[0].split(',')[2]] += 1
         except KeyError:
             champion_to_wins[line.split('"')[0].split(',')[2]] = 1
+    return champion_to_wins
+
+
+def assign_champion_to_losses(data):
+    """Return dictionary with champion name to number of losses."""
+    champion_to_losses = {}
+    for line in data:
+        score_digits = [int(char) for char in line.split('"')[1] if char in '1234567890']
         for i in range(len(score_digits) // 2):
             if (score_digits[i * 2] - score_digits[i * 2 + 1]) < 0:
                 try:
                     champion_to_losses[line.split('"')[0].split(',')[2]] += 1
                 except KeyError:
                     champion_to_losses[line.split('"')[0].split(',')[2]] = 1
-    return champion_to_wins, champion_to_losses
+    return champion_to_losses
 
 
 def read_file():
