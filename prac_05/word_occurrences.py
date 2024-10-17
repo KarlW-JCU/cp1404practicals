b338d252cb453be3aa265a1aebd366559433ef0e
KarlW-JCU <karl.wellington@my.jcu.edu.au>
2024-10-17 12:23:52 +1000

    Add word occurrences program

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,17 @@
+"""
+Word Occurrences
+Estimate: 30 minutes
+Actual:   12 minutes
+"""
+
+string = input("Text: ")
+word_to_count = {}
+for word in string.split():
+    try:
+        word_to_count[word] += 1
+    except KeyError:
+        word_to_count[word] = 1
+
+longest_word = max([len(word) for word in word_to_count.keys()])
+for item in sorted(word_to_count):
+    print(f"{item:<{longest_word}} : {word_to_count[item]:>2}")
