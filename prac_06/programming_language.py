b4291fbb4249d3f63c00daf93b65123ac3a0d070
KarlW-JCU <karl.wellington@my.jcu.edu.au>
2024-10-31 11:55:14 +1000

    Add language programs

---
+++ prac_06/programming_language.py
@@ -0,0 +1,22 @@
+"""Programming Language Class"""
+
+
+class ProgrammingLanguage:
+    """Programming Language Class: name, typing, reflection, year."""
+
+    def __init__(self, name="", typing="", reflection="", year=0):
+        """Construct a programming_language object."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        """Return class object summary."""
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+
+    def is_dynamic(self):
+        """Determine if language is dynamic."""
+        if self.typing == "Dynamic":
+            return True
+        return False
