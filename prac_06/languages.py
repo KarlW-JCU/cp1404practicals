a1c5b3fed1f1dff94203c71d29d5dafdd4c18302
KarlW-JCU <karl.wellington@my.jcu.edu.au>
2024-11-07 13:52:44 +1000

    Refactor languages print loop

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -13,4 +13,6 @@ print(python)
 
 languages = [python, ruby, visual_basic]
 print("The dynamically typed languages are:")
-[print(language.name) for language in languages if language.is_dynamic()]
+for language in languages:
+    if language.is_dynamic():
+        print(language.name)
