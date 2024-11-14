c987700688462fb97c4bd500028af23dbb48cd4d
KarlW-JCU <karl.wellington@my.jcu.edu.au>
2024-11-14 13:09:24 +1000

    Add miles to km conversion app base

---
+++ prac_08/convert_miles_km.py
@@ -0,0 +1,17 @@
+"""Miles to Km Conversion App"""
+
+from kivy.app import App
+from kivy.lang import Builder
+
+
+class ConvertMilesKm(App):
+    """Miles to Km Conversion App"""
+
+    def build(self):
+        """"""
+        self.title = "Convert Miles to Kilometres"
+        self.root = Builder.load_file("convert_miles_km.kv")
+        return self.root
+
+
+ConvertMilesKm().run()
