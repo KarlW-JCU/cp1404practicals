0a79ff1e5ef5105a3908ece6adbfef68ed80e93c
KarlW-JCU <karl.wellington@my.jcu.edu.au>
2024-11-14 14:03:25 +1000

    Complete error handling: miles to km conversion app

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -5,6 +5,8 @@ from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.properties import StringProperty
 
+MILES_TO_KM = 1.6093
+
 
 class ConvertMilesKm(App):
     """Miles to Km Conversion App"""
@@ -13,7 +15,7 @@ class ConvertMilesKm(App):
 
     def build(self):
         """"""
-        Window.size = (720, 400)
+        Window.size = (400, 200)
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file("convert_miles_km.kv")
         self.input_miles = "0"
@@ -21,20 +23,21 @@ class ConvertMilesKm(App):
         return self.root
 
     def handle_convert(self, value):
-        """"""
+        """Convert input miles to kilometres."""
         try:
-            result = round(float(value) * 1.6093, 4)
+            result = round(float(value) * MILES_TO_KM, 4)
             self.output_km = str(result)
         except ValueError:
-            pass
+            self.output_km = "0.0"
 
     def handle_increment(self, field, value):
-        """"""
+        """Increment field by value."""
         try:
             result = int(field) + value
             self.input_miles = str(result)
         except ValueError:
-            pass
+            result = 0 + value
+            self.input_miles = str(result)
 
 
 ConvertMilesKm().run()
