e1800f132c074f747eb2edae244aa26ab1d60073
KarlW-JCU <karl.wellington@my.jcu.edu.au>
2024-11-14 13:17:50 +1000

    Complete layout: miles to km conversion app

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -2,6 +2,7 @@
 
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.core.window import Window
 
 
 class ConvertMilesKm(App):
@@ -9,6 +10,7 @@ class ConvertMilesKm(App):
 
     def build(self):
         """"""
+        Window.size = (720, 400)
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file("convert_miles_km.kv")
         return self.root
