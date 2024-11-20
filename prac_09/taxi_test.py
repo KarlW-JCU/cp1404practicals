6c2d0788c3a52e0860ac27dc2848653466a0bb61
KarlW-JCU <karl.wellington@my.jcu.edu.au>
2024-11-21 09:32:11 +1000

    Complete taxi tests

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -1,2 +1,9 @@
 """Taxi (Car) Test Program"""
+from prac_09.taxi import Taxi
 
+my_taxi = Taxi(name="Prius 1", fuel=100)
+my_taxi.drive(40)
+print(my_taxi)
+my_taxi.start_fare()
+my_taxi.drive(100)
+print(my_taxi, my_taxi.current_fare_distance)
