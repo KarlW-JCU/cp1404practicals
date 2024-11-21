251546b600058e3c68b2c3366d94ba38834cb442
KarlW-JCU <karl.wellington@my.jcu.edu.au>
2024-11-21 11:29:43 +1000

    Enhance taxi class and subclass

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -4,8 +4,8 @@ from prac_09.silver_service_taxi import SilverService
 
 my_taxi = SilverService(name="Mercedes", fuel=200, fanciness=2.5)
 print(my_taxi)
-my_taxi.drive(40)
-print(my_taxi, "Fare: $", my_taxi.get_fare())
+my_taxi.drive(11)
+print(my_taxi, f"Fare: ${my_taxi.get_fare():.2f}")
 my_taxi.add_fuel(50)
 my_taxi.start_fare()
 print(my_taxi)
